379774d422be2785266aa739415e31c6e4947021
0311869uaslp-a11y <0311869uaslp@gmail.com>
2025-10-13 14:24:49 +0200

    code

--- tp2/action.js
+++ tp2/action.js
@@ -1,25 +1,25 @@
 window.onload = () => {
-  // Récupérer les données envoyées par le formulaire
+  
   const paramsString = document.location.search; 
   const searchParams = new URLSearchParams(paramsString);
 
-  // Boucle sur chaque paramètre
+ 
   for (const param of searchParams) {
-    console.log(param); // Affichage pour debug
+    console.log(param); 
 
     const elementId = param[0];
     const elementValue = param[1];
     const element = document.getElementById(elementId);
 
     if (element !== null) {
-      element.textContent = elementValue; // Mettre à jour le contenu
+      element.textContent = elementValue; 
     }
 
-    // Lien Google Maps pour l'adresse
+   
     if (param[0] === "address") {
       element.href = `https://www.google.com/maps/search/?api=1&query=${elementValue}`;
     }
-    // Lien mailto pour l'email
+    
     else if (param[0] === "email") {
       element.href = `mailto:${elementValue}?subject=Hello!&body=What's up?`;
     }
